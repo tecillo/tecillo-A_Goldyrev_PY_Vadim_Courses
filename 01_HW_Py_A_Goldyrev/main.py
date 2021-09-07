97af1a0008415c9055b5411e3e421cabe0e43f1d
tecillo <tecilloQA@gmail.com>
2021-09-07 15:19:52 +0300

    hw_1

--- 01_HW_Py_A_Goldyrev/main.py
+++ 01_HW_Py_A_Goldyrev/main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+st = "My string"
+i = 1
+flo = 1.5
+b = bytes(4)
+l = ["ok", "google", 56, 79]
+tu = ("I", "am", "hungry", 77)
+se = {"hello", 777, 953, 777}
+fse = frozenset("666")
+dic = {"name" : "Killabeez"}
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print(
+    "st - ", st, type(st), "\n", "i - ", i, type(i), "\n", "flo - ", flo, type(flo), "\n", "b - ", b, type(b),
+    "\n", "l - ", l, type(l), "\n", "tu - ", tu, type(tu),"\n", "se - ", se, type(se), "\n", "fse - ", fse, type(fse)
+)
 
+unoStr = "Andrey"
+dosStr = "Goldyrev"
+tres = unoStr + ' ' + dosStr
+print(tres)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+unoIn = 41
+dosIn = 12
+tresIn = unoIn + dosIn
+quatroIn = unoIn * dosIn
+cincoIn = unoIn / dosIn
+seisIn = unoIn // dosIn
+sieteIn = unoIn % dosIn
+print(tresIn, " ", quatroIn, " ", cincoIn, " ", seisIn, " ", sieteIn)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
